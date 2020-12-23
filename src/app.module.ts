0752ce79cd3ed8b2312da0e40cccea6e697464c5
durban.zhang <durban.zhang@gmail.com>
2020-12-23 15:05:04 +0800

    添加文章控制器

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { ArticleController } from './article/article.controller';
 
 @Module({
   imports: [CatsModule],
-  controllers: [AppController],
+  controllers: [AppController, ArticleController],
   providers: [AppService],
 })
 export class AppModule {}
